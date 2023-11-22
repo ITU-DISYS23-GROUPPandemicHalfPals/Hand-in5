9b9034d866b5887b8cd2d2c5d264c71c29596ef2
Patrick Handberg Jessen <patj@itu.dk>
2023-11-22 12:22:36 +0100

    Implemented bidding on the server
    Co-Authored-By: Wrenaeris <119424078+Wrenaeris@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -3,97 +3,118 @@ package main
 import (
 	"auction/auction"
 	"context"
-	"flag"
+	"errors"
+	"fmt"
 	"log"
 	"net"
-	"strconv"
-	"time"
+	"sync"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
-var port = flag.Int("port", 5000, "The port of the server")
-
 type server struct {
-	Port            int
-	CoordinatorPort int
-	Ports           []int
+	HighestBidderId   int
+	HighestBidderName string
+	HighestBid        int
 
-	ElectionChannel chan bool
-	Clients         map[int]auction.ElectionClient
-	BiggerClients   map[int]auction.ElectionClient
+	TimeLeft float32
+	Done     bool
 
-	auction.UnimplementedElectionServer
+	BidMutex sync.Mutex
+
+	auction.UnimplementedAuctionServer
 }
 
-func Server(port int) *server {
-	var ports []int
-
-	for i := 5000; i <= 5002; i++ {
-		ports = append(ports, i)
-	}
-
-	return &server{
-		Port:            port,
-		CoordinatorPort: 0,
-		Ports:           ports,
-
-		ElectionChannel: make(chan bool, 10),
-		Clients:         make(map[int]auction.ElectionClient),
-		BiggerClients:   make(map[int]auction.ElectionClient),
-	}
+func Server() *server {
+	return &server{}
 }
 
 func main() {
-	flag.Parse()
-
-	n := Server(*port)
-
-	ctx := context.Background()
-
-	go n.server(ctx)
-	n.client(ctx)
+	s := Server()
+	s.server()
 }
 
-func (n *server) server(ctx context.Context) {
-	s := grpc.NewServer()
-	auction.RegisterElectionServer(s, n)
+func (s *server) server() {
+	server := grpc.NewServer()
+	auction.RegisterAuctionServer(server, s)
 
-	listener, error := net.Listen("tcp", ":"+strconv.Itoa(n.Port))
+	listener, error := net.Listen("tcp", ":5000")
 	if error != nil {
 		log.Fatalf("Failed to listen: %s", error)
 	}
 
-	error = s.Serve(listener)
+	s.timer()
+
+	error = server.Serve(listener)
 	if error != nil {
 		log.Fatalf("Failed to serve: %s", error)
+
 	}
 }
 
-func (n *server) Election(_ context.Context, request *auction.ElectionMessage) (*auction.Response, error) {
-	n.ElectionChannel <- true
+func (s *server) Bid(_ context.Context, request *auction.BidRequest) (*auction.BidResponse, error) {
+	error := s.auction(request)
+	if error != nil {
+		return &auction.BidResponse{}, error
+	}
+
+	return &auction.BidResponse{}, nil
+}
+
+func (s *server) Result(_ context.Context, request *auction.ResultRequest) (*auction.ResultResponse, error) {
+	if s.Done {
+
+	} else {
+
+	}
+}
+
+func (s *server) timer() {
+	s.Done = true
+}
+
+func (s *server) auction(bid *auction.BidRequest) error {
+	if s.Done {
+		return errors.New("Auction is done.")
+	}
+
+	s.BidMutex.Lock()
+	defer s.BidMutex.Unlock()
+	if int(bid.Amount) > s.HighestBid {
+		s.HighestBidderId = int(bid.Id)
+		s.HighestBidderName = bid.Name
+		s.HighestBid = int(bid.Amount)
+	} else {
+		return errors.New(fmt.Sprintf("Your bid has to be higher than the biggest bid. Your bid: %d. Highest bid: %d.", bid.Amount, s.HighestBid))
+	}
+
+	return nil
+}
+
+/*
+func (s *server) Election(_ context.Context, request *auction.ElectionMessage) (*auction.Response, error) {
+	s.ElectionChannel <- true
 
 	return &auction.Response{}, nil
 }
 
-func (n *server) Coordinator(_ context.Context, request *auction.CoordinatorMessage) (*auction.Response, error) {
-	n.CoordinatorPort = int(request.Port)
+func (s *server) Coordinator(_ context.Context, request *auction.CoordinatorMessage) (*auction.Response, error) {
+	s.CoordinatorPort = int(request.Port)
 
 	return &auction.Response{}, nil
 }
 
-func (n *server) client(ctx context.Context) {
-	go n.dialServers()
-	n.broadcastElection(ctx)
+func (s *server) client(ctx context.Context) {
+	go s.dialServers()
+	s.broadcastElection(ctx)
 }
 
-func (n *server) dialServers() {
+func (s *server) dialServers() {
 	for {
 		time.Sleep(time.Second)
 
-		for _, port := range n.Ports {
-			_, ok := n.Clients[port]
+		for _, port := range s.Ports {
+			_, ok := s.Clients[port]
 
 			if ok {
 				continue
@@ -106,25 +127,25 @@ func (n *server) dialServers() {
 
 			client := auction.NewElectionClient(connection)
 
-			n.Clients[port] = client
+			s.Clients[port] = client
 
-			if port > n.Port {
-				n.BiggerClients[port] = client
+			if port > s.Port {
+				s.BiggerClients[port] = client
 			}
 		}
 	}
 }
 
-func (n *server) broadcastElection(ctx context.Context) {
+func (s *server) broadcastElection(ctx context.Context) {
 	for {
-		<-n.ElectionChannel
-		n.startElection(ctx)
+		<-s.ElectionChannel
+		s.startElection(ctx)
 	}
 }
 
-func (n *server) startElection(ctx context.Context) {
+func (s *server) startElection(ctx context.Context) {
 	response := false
-	for _, client := range n.BiggerClients {
+	for _, client := range s.BiggerClients {
 		ctx, cancel := context.WithTimeout(ctx, time.Second)
 		_, error := client.Election(ctx, &auction.ElectionMessage{})
 
@@ -137,13 +158,14 @@ func (n *server) startElection(ctx context.Context) {
 
 	if !response {
 		coordinatorMessage := &auction.CoordinatorMessage{
-			Port: int32(n.Port),
+			Port: int32(s.Port),
 		}
 
-		for _, client := range n.Clients {
+		for _, client := range s.Clients {
 			ctx, cancel := context.WithTimeout(ctx, time.Second)
 			_, _ = client.Coordinator(ctx, coordinatorMessage)
 			cancel()
 		}
 	}
 }
+*/
